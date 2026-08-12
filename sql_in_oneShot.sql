18fbe26de3a818d66bf6504b6c26802f39b9b6c9
Manoj Kumar <contact@manojkumar-dev.com>
2026-08-12 23:15:58 +0200

    Update sql_in_oneShot.sql

--- sql_in_oneShot.sql
+++ sql_in_oneShot.sql
@@ -73,3 +73,70 @@ SELECT DATABASE();
 
 -- drop database if exists
 DROP DATABASE IF EXISTS gisma4;
+
+
+-- create a table for current database
+CREATE TABLE calender (
+month DATE NOT NUll
+);
+
+
+-- insert data into table
+INSERT INTO calender 
+(month)
+VALUES
+('2026-01-06'),
+('2026-01-06'),
+('2026-01-06'),
+('2026-01-06'
+);
+
+
+-- show table data
+SELECT * FROM calender;
+
+
+-- rename database in MySQL;
+-- mysql -u root -p
+-- password
+-- mysqldump -u root -p oldDB > oldDB_backup.sql
+-- CREATE DATABASE newDB;
+-- mysql -u root -p newDB < oldDB_backup.sql
+-- DROP DATABASE oldDB;
+
+
+-- show all databases
+SHOW DATABASES;
+
+
+-- show databases by patter(exact database name)
+SHOW DATABASES LIKE 'coll';
+
+
+-- show database by patters (% -- add 0 or more character to match)
+SHOW DATABASES LIKE 'coll%';
+
+
+-- show database by patters (exactly one character)
+SHOW DATABASES LIKE 'coll_';
+
+
+-- show all database by patters
+SHOW DATABASES  WHERE `DATABASE` LIKE 'coll%';
+
+
+-- show exact database using condition
+SHOW DATABASES WHERE `DATABASE` = 'college1';
+
+
+-- backup MySQL
+
+
+-- create a table customers
+CREATE TABLE customers (
+id INT NOT NULL,
+name VARCHAR(30),
+age INT NOT NULL,
+address CHAR(50),
+salary DECIMAL(18, 2)
+);
