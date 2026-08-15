5755f63886678cf968f91340b4c642a8facd03a6
Manoj Kumar <contact@manojkumar-dev.com>
2026-08-15 23:46:57 +0200

    Update operators.sql

--- operators.sql
+++ operators.sql
@@ -157,3 +157,33 @@ VALUES
 SELECT * from operators WHERE age NOT IN (25, 23, 22);
 
 
+
+-- ------------------------------
+-- WHERE Clause with LIKE Operator
+
+USE temp2;
+CREATE TABLE operators (
+   id INT NOT NULL,
+   name VARCHAR (20) NOT NULL,
+   age INT NOT NULL,
+   address CHAR (25),
+   salary DECIMAL (18, 2),
+   PRIMARY KEY (ID)
+);
+
+
+INSERT INTO operators 
+(id, name, age, address, salary)
+VALUES
+(1, 'Ramesh', 32, 'Ahmedabad', 2000.00),
+(2, 'Khilan', 25, 'Delhi', 1500.00),
+(3, 'Kaushik', 23, 'Kota', 2000.00),
+(4, 'Chaitali', 25, 'Mumbai', 6500.00),
+(5, 'Hardik', 27, 'Bhopal', 8500.00),
+(6, 'Komal', 22, 'Hyderabad', 4500.00),
+(7, 'Muffy', 24, 'Indore', 10000.00);
+
+
+SELECT * FROM operators WHERE NAME LIKE 'K___%';
+
+
